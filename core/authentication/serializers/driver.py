05c1b233b9b90e4545236fb78b4b84b1e29cc81a
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-05-09 13:45:12 -0300

    Fix: Atualizar DriverSerializer para remover campos de leitura. O campo 'user.id' e 'user.password' foram removidos da lista de read_only_fields, simplificando a serialização do modelo Driver. [Refs: #9]

--- core/authentication/serializers/driver.py
+++ core/authentication/serializers/driver.py
@@ -10,4 +10,4 @@ class DriverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Driver
         fields = ['id', 'user', 'cnh', 'active']
-        read_only_fields = ['id', 'user.id', 'user.password']
+        read_only_fields = ['id']
