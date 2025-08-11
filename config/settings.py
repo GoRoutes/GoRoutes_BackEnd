d5dcda4ba562fb354c05daa1db9d8960e35742af
AnthonyLoche <anthonylocheifc@gmail.com>
2025-08-11 14:19:52 -0300

    FIX: Ajustar configuração do banco de dados para o modo de desenvolvimento e migração

--- config/settings.py
+++ config/settings.py
@@ -80,19 +80,20 @@ REST_FRAMEWORK = {
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-if MODE == 'MIGRATE':
-    DATABASES = {
-        'default': dj_database_url.config(
-            default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-            conn_max_age=600,
-        )
-    }
-else:  # DEVELOPMENT ou qualquer outro valor padrão
+if MODE == 'DEVELOPMENT':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default='sqlite:///db.sqlite3',
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
 
 AUTH_PASSWORD_VALIDATORS = [
--- core/websocket/routing.py
+++ core/websocket/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/localizacao/(?P<motorista_id>\w+)/$', consumers.LocalizacaoConsumer.as_asgi()),
+    re_path(r'ws/localizacao/(?P<user_id>\w+)/$', consumers.LocalizacaoConsumer.as_asgi()),
 ]
