84e678f3803b889907f07609232009500110f1ba
AnthonyLoche <anthonylocheifc@gmail.com>
2025-08-11 14:29:06 -0300

    FIX: Remover breakpoint do método create_passenger

--- core/authentication/views/handlers/passenger_handlers.py
+++ core/authentication/views/handlers/passenger_handlers.py
@@ -32,7 +32,6 @@ def create_passenger(request):
     addresses_data = validated_data.pop('addresses', [])
     student_data = validated_data.pop('student_data', None)
     
-    breakpoint()
     user = User.objects.create_user(
         username=user_data['username'],
         name=user_data['name'],
