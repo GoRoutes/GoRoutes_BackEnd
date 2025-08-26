bac627bd5240be9160e8614a1145592303c8c18e
AnthonyLoche <anthonylocheifc@gmail.com>
2025-08-25 21:37:21 -0300

    ATUALIZAR: Aumentar o tamanho máximo do campo CPF para 20 caracteres

--- core/authentication/models/responsible.py
+++ core/authentication/models/responsible.py
@@ -3,7 +3,7 @@ from core.authentication.models import User
 
 class Responsible(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='responsible')
-    cpf = models.CharField(max_length=11, unique=True)
+    cpf = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return f'{self.user.username} - {self.cpf}'
