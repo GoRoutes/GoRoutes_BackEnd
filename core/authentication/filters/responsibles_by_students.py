fd3b00c5c07b383924c0b4279f8ab22cfb1dbabe
AnthonyLoche <anthonylocheifc@gmail.com>
2025-09-17 23:04:43 -0300

    FEAT: add passenger filter to CombinedUserFilter and remove unused image files

--- core/authentication/filters/responsibles_by_students.py
+++ core/authentication/filters/responsibles_by_students.py
@@ -4,6 +4,7 @@ from core.authentication.models import User
 class CombinedUserFilter(django_filters.FilterSet):
     is_driver = django_filters.BooleanFilter(method='filter_is_driver')
     responsible_id = django_filters.NumberFilter(field_name='passenger__student_data__responsible__id')
+    is_passenger = django_filters.BooleanFilter(method='filter_is_passenger')
 
     def filter_is_driver(self, queryset, name, value):
         if value:
@@ -11,6 +12,12 @@ class CombinedUserFilter(django_filters.FilterSet):
         else:
             return queryset.filter(driver__isnull=True)
 
+    def filter_is_passenger(self, queryset, name, value):
+        if value:
+            return queryset.exclude(passenger__isnull=True)
+        else:
+            return queryset.filter(passenger__isnull=True)
+
     class Meta:
         model = User
-        fields = ['is_driver', 'responsible_id']
+        fields = ['is_driver', 'responsible_id', 'is_passenger']
